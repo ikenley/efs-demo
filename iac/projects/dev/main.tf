e6c7eeb649a453272a2b36c3af9c619cffe8948a
ikenley <ikenley6@gmail.com>
2024-08-31 11:02:08 -0400

    init

---
+++ iac/projects/dev/main.tf
@@ -0,0 +1,40 @@
+# ------------------------------------------------------------------------------
+# efs-demo
+# Example of AWS Step Function with various integrations
+# ------------------------------------------------------------------------------
+
+terraform {
+  required_version = ">= 0.14"
+
+  backend "s3" {
+    profile = "terraform-dev"
+    region  = "us-east-1"
+    bucket  = "924586450630-terraform-state"
+    key     = "efs/dev/terraform.tfstate.json"
+  }
+}
+
+provider "aws" {
+  alias   = "primary"
+  region  = "us-east-1"
+  profile = "terraform-dev"
+}
+
+provider "aws" {
+  alias   = "failover"
+  region  = "us-west-2"
+  profile = "terraform-dev"
+}
+
+# ------------------------------------------------------------------------------
+# Resources
+# ------------------------------------------------------------------------------
+
+module "main" {
+  source = "../../modules/main_root"
+
+  namespace = "ik"
+  env       = "dev"
+  is_prod   = false
+
+}
